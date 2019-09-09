60e0449c13c8f78ede626fa7e2fdf2be24969fdb
Claire Dong <claire.dong@duke.edu>
2019-09-09 12:28:44 -0400

    Design cholesterol interface

--- calculator.py
+++ calculator.py
@@ -6,6 +6,14 @@ def check_HDL(HDL_result):
     else:
         return "low"
 
+def cholesterol_interface():
+    print("Cholesterol check")
+    chol_input = input("Enter your cholesterol test result: ")
+    chol_data = chol_input.split("=")
+    if chol_data[0] = "HDL":
+        result = check_HDL(chol_data[1])
+        print("THe result is {}".format(result))
+
 def interface():
         print("My calculator program")
         keep_running = True

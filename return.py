da05eccb5bd2804b41663b92f50fcd73c089b153
Claire Dong <claire.dong@duke.edu>
2019-09-18 12:55:44 -0400

    added requirements

---
+++ return.py
@@ -0,0 +1,14 @@
+def my_function(x):
+    a = x + 5
+    if a < 0:
+        return "cannot be negative"
+    else:
+        return a
+
+if __name__ == "__main__":
+    i = -20
+    answer = my_function(i)
+    if answer == None:
+        print("It returned nothing")
+    else:
+        print(answer)

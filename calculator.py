7525001f87529bc85b37fc1435ca709d09bbba76
Claire Dong <claire.dong@duke.edu>
2019-09-17 22:19:03 -0400

    added total cholesterol check

--- calculator.py
+++ calculator.py
@@ -1,3 +1,22 @@
+def check_total_cholesterol(total_cholesterol):
+    if total_cholesterol < 200:
+        return "normal"
+    elif 200<=total_cholesterol<239:
+        return "borderline high"
+    else:
+        return "high"
+
+def total_cholesterol_interface():
+    print("Total cholesterol check")
+    chol_input = input("Enter your total cholesterol result (total=x): ")
+    chol_data = chol_input.split("=")
+    if chol_data[0] == "total":
+        result = check_HDL(int(chol_data[1]))
+        print("The result is {}".format(result))
+    else:
+        print("bad input")
+
+
 def check_HDL(HDL_result):
     if HDL_result >= 60:
         return "Normal"
@@ -17,7 +36,7 @@ def check_LDL(LDL_result):
         return "Very high"
 
 def cholesterol_interface():
-    print("Cholesterol check")
+    print("HDL/LDL check")
     chol_input = input("Enter your cholesterol test result: ")
     chol_data = chol_input.split("=")
     if chol_data[0] in ["HDL","LDL"]:
@@ -35,13 +54,18 @@ def interface():
         keep_running = True
         while keep_running:
             print("Option: ")
-            print("1 - Cholesterol Check")
+            print("1 - HDL/LDL Check")
+            print("2 - Total Cholesterol Check")
             print("9 - Quit")
             choice = input("Enter your choice: ")
             if choice == '9':
                 keep_running = False
             elif choice == '1':
                 cholesterol_interface()
+            elif choice == '2':
+                total_cholesterol_interface()
+            else:
+                print("bad input")
         return
 
 if __name__ == "__main__":
